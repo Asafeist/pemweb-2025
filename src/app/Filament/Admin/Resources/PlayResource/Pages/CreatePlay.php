8fa70df1834809342f8d8fdea70f9ca2aea7201b
Asafeist <rizky.nurhafiizh@gmail.com>
2025-10-04 08:32:09 +0700

    test

---
+++ src/app/Filament/Admin/Resources/PlayResource/Pages/CreatePlay.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Admin\Resources\PlayResource\Pages;
+
+use App\Filament\Admin\Resources\PlayResource;
+use Filament\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreatePlay extends CreateRecord
+{
+    protected static string $resource = PlayResource::class;
+}
